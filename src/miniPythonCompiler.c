9a16c90ea9cabf4cf598fc1bdd8f1bb19115256d
Vitor Pereira <pereira.alves.vitor@icloud.com>
2026-04-03 17:47:23 -0300

    Trabalhando na análise léxica

--- src/miniPythonCompiler.c
+++ src/miniPythonCompiler.c
@@ -10,6 +10,18 @@
 
 #define FILE_TEMP_PATH "../temp/"
 
+typedef struct Token {
+    char *type;
+    char *value;
+} Token;
+
+typedef struct SymbolTable {
+    int id[256];
+    char *name[256];
+    char *type[256];
+
+} SymbolTable;
+
 /** @brief Recebe o arquivo de entrada e procede para validá-lo, retornando 0 se for válido e 1 caso contrário.
  *  @param argc número de parâmetros recebidos na linha de comando.
  *  @param argv vetor com os parâmetros recebidos na linha de comando
@@ -33,6 +45,12 @@ FILE *createFileCopy(int argc, char *argv[]);
 int removeCharacters(char *fileCopyPath, char characterToRemove);
 
 int slice(char *newString, char *string, char caracterToLookFor, int length);
+
+int getNextToken();
+
+int insertTokenIntoSymbolTable(Token token, SymbolTable *symbolTable);
+
+int fillSymbolTable();
 
 int main(int argc, char *argv[]) {
 
@@ -63,6 +81,7 @@ int main(int argc, char *argv[]) {
     }
 
     char fileCopyPath[256];
+
     char fileName[256];
 
     int result = slice(fileName, argv[1], '/', strlen(argv[1]));
@@ -79,13 +98,15 @@ int main(int argc, char *argv[]) {
 
     printf("Caminho do arquivo copiado: %s\n", fileCopyPath);
 
+    int didFillSymbolTableSucceed = fillSymbolTable();
+
     int didCommentsRemovalProcessSucceed = removeCharacters(fileCopyPath, '#');
     
-    int didWhiteSpacesRemovalProcessSucceed = removeCharacters(fileCopyPath, ' ');
-    
     int didTabsRemovalProcessSucceed = removeCharacters(fileCopyPath, '\t');
-
+    
     int didLineBreaksRemovalProcessSucceed = removeCharacters(fileCopyPath, '\n');
+
+    int didWhiteSpacesRemovalProcessSucceed = removeCharacters(fileCopyPath, ' ');
 
     if (didCommentsRemovalProcessSucceed != 0) {
 
@@ -281,13 +302,88 @@ int removeCharacters(char *fileCopyPath, char characterToRemove) {
 
 }
 
-
 int getNextToken() {
 
+    FILE *copy = fopen("./output/example-1.txt", "rb");
 
+    FILE *commentariesIdentified = fopen("./output/commentariesIdentified.txt", "wb");
 
+    if (!copy) return 1;
 
+    int i = 0;
 
+    // Comments
+    goto comments;
+
+    comments:
+
+        char commentariesBuffer[256];
+
+        int c = fgetc(copy);
+
+        goto Q0;
+
+        Q0:
+
+            if (c != EOF && c == '#') {
+
+                commentariesBuffer[i] = c;
+
+                c = fgetc(copy);
+
+                goto Q1;
+
+            } else {
+
+                printf("[ AVISO ] Comentário não encontrado!\n");
+
+                goto comment_end;
+
+            }
+            
+        Q1:
+
+            if (c != EOF && c != '\n') {
+
+                commentariesBuffer[i] = c;
+                    
+                i++;
+                
+                goto Q1;
+    
+            } else if (c == '\n') {
+
+                commentariesBuffer[i] = '\0';
+
+                fputs(commentariesBuffer, commentariesIdentified);
+    
+                printf("[ SUCESSO ] Comentário aceito!\n");
+
+                goto comment_end;
+                
+            } else {
+                
+                printf("[ ERRO ] Comentário não aceito!");
+                
+                goto comment_end;
+                
+            }
+            
+    comment_end:
+            
+    commentariesBuffer[0] = '\0';
+    i = 0;
+
+    fclose(copy);
+    fclose(commentariesIdentified);
+
+    return 0;
+    
+}
+
+int fillSymbolTable() {
+
+    getNextToken();
 
     return 0;
 
